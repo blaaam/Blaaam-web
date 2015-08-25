daf7dc94b388d6cd544b51c8edd7d3de57f3e650
SmokeY <staff.migets@gmail.com>
2015-08-25 17:56:11 +0300

    Added some new lines, not used yet!

--- include/config.php
+++ include/config.php
@@ -2,30 +2,62 @@
 
 //  CONFIGS
 
-// define ("PATH_LOGINSERVER", "C:/");
-// define ("PATH_GAMESERVER", "C:/");
-// define ("SERVER_NAME", "L2ERTHEIA");
-// define ("RATE_XP", "100x");
-// define ("RATE_SP", "100x");
-// define ("RATE_DROP", "3x");
-// define ("RATE_ADENA", "2x");
-// define ("RATE_SPOIL", "1x");
-// define ("PATH_LOGO", "");
-// define ("PATH_SLIDE1", "_imagens/img/dummy-640x310-1.jpg");
-// define ("PATH_SLIDE2", "_imagens/img/dummy-640x310-2.jpg");
-// define ("PATH_SLIDE3", "_imagens/img/dummy-640x310-3.jpg");
-// define ("PATH_VOTE1", "");
-// define ("PATH_VOTE2", "");
-// define ("PATH_VOTE3", "");
-// define ("PATH_VOTE4", "");
-// define ("PATH_VOTE5", "");
-// define ("PATH_VOTE6", "");
-// define ("CREDITS", "");
+// define ("PATH_LOGINSERVER", "C:/"); // Path to Login server log file
+// define ("PATH_GAMESERVER", "C:/"); // Path to Game server log file
+// define ("SERVER_NAME", "L2ERTHEIA"); // Write your server name here
+// define ("RATE_XP", "100x"); // Server rate XP, will be displayed in top-bar at web pages
+// define ("RATE_SP", "100x"); // Server rate SP, will be displayed in top-bar at web pages
+// define ("RATE_DROP", "3x"); // Server rate DROP, will be displayed in top-bar at web pages
+// define ("RATE_ADENA", "2x"); // Server rate ADENA, will be displayed in top-bar at web pages
+// define ("RATE_SPOIL", "1x"); // Server rate SPOIL, will be displayed in top-bar at web pages
+// define ("PATH_LOGO", ""); // Path to logo file (Not used currently)
+// define ("PATH_SLIDE1", "_imagens/img/dummy-640x310-1.jpg"); // Path to image for first slide in web page
+// define ("PATH_SLIDE2", "_imagens/img/dummy-640x310-2.jpg"); // Path to image for second slide in web page
+// define ("PATH_SLIDE3", "_imagens/img/dummy-640x310-3.jpg"); // Path to image for third slide in web page
+// define ("PATH_VOTE1", ""); // VOTE Site banner image for first vote (Only image)
+// define ("PATH_VOTE2", ""); // VOTE Site banner image for second vote (Only image)
+// define ("PATH_VOTE3", ""); // VOTE Site banner image for third vote (Only image)
+// define ("PATH_VOTE4", ""); // VOTE Site banner image for forth vote (Only image)
+// define ("PATH_VOTE5", ""); // VOTE Site banner image for five vote (Only image)
+// define ("PATH_VOTE6", ""); // VOTE Site banner image for six vote (Only image)
+// define ("SITE_VOTE1", ""); // VOTE Site banner address link for first vote (Only address)
+// define ("SITE_VOTE2", ""); // VOTE Site banner address link for second vote (Only address)
+// define ("SITE_VOTE3", ""); // VOTE Site banner address link for third vote (Only address)
+// define ("SITE_VOTE4", ""); // VOTE Site banner address link for forth vote (Only address)
+// define ("SITE_VOTE5", ""); // VOTE Site banner address link for five vote (Only address)
+// define ("SITe_VOTE6", ""); // VOTE Site banner address link for six vote (Only address)
+// define ("CREDITS", ""); // Credits which will be displayed at bottom at web in the footer section
+// define ("DOWNLOAD_SYS_1", ""); // Site to download System map mirror 1
+// define ("DOWNLOAD_SYS_2", ""); // Site to download System map mirror 2
+// define ("DOWNLOAD_ANIM_1", ""); // Site to download Animations map mirror 1
+// define ("DOWNLOAD_ANIM_2", ""); // Site to download Animations map mirror 2
+// define ("DOWNLOAD_SYSTEX_1", ""); // Site to download System textures map mirror 1
+// define ("DOWNLOAD_SYSTEX_2", ""); // Site to download System textures map mirror 2
+// define ("DOWNLOAD_FULL_EXE_1", ""); // Site to download full patch installation mirror 1
+// define ("DOWNLOAD_FULL_EXE_2", ""); // Site to download full patch installation mirror 2
+// define ("DOWMLOAD_FULL_RAR_1", ""); // Site to download full patch archived mirror 1
+// define ("DOWNLOAD_FULL_RAR_2", ""); // Site to download full patch archived mirror 2
+// define ("DOWNLOAD_CLIENT_1", ""); // Site to download game client mirror 1
+// define ("DOWNLOAD_CLIENT_2", ""); // Site to download game client mirror 2
+// define ("SITE_TITLE", "This is my L2 Server"); // Title for meta tag 
+// define ("SITE_EMAIL", "mail@gmail.com"); // Site e-mail
+// define ("item_id", "item_id"); // Database (Will add more of database entry's here)
+// define ("DropItemId", "57"); // Not sure where this is used, will check later!
+// define ("MAX_ENCHANT", "25"); // Max enchant rate for your server
+// define ("", "");
+// define ("", "");
+// define ("", "");
+// define ("", "");
+// define ("", "");
+// define ("", "");
+// define ("", "");
 // define ("", "");
 // define ("", "");
 // define ("", "");
 
 
 
+
+$item_id = "item_id"; // From old configuration file (Original = $item_id = 'item_id';)
 
 ?>
