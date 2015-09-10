9ba5aa3031ff6b0b49765c509a68c90259f51b74
SmokeY <staff.migets@gmail.com>
2015-09-10 16:56:01 +0300

    added shoutbox

--- database/upload_to_your_database.sql
+++ database/upload_to_your_database.sql
@@ -109,3 +109,12 @@ CREATE TABLE IF NOT EXISTS `web_report` (
   `descricao` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `shout_box` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(60) NOT NULL,
+  `message` varchar(100) NOT NULL,
+  `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip_address` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
--- index.php
+++ index.php
@@ -74,6 +74,154 @@ include_once 'languages/'.$lang_file;
 	<link rel="stylesheet" type="text/css" href="css/slideshow.css"/>
 	<link rel="stylesheet" type="text/css" href="css/user.login.css">
 	<link rel="stylesheet" type="text/css" href="js/lightbox/css/lightbox.css" />
+	<script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+	<style type="text/css">
+<!--
+.shout_box {
+	background: #627BAE;
+	width: 260px;
+	overflow: hidden;
+	position: fixed;
+	bottom: 0;
+	right: 20%;
+	z-index:9;
+}
+.shout_box .header .close_btn {
+	background: url(images/close_btn.png) no-repeat 0px 0px;
+	float: right;
+	width: 15px;
+	height: 15px;
+}
+.shout_box .header .close_btn:hover {
+	background: url(images/close_btn.png) no-repeat 0px -16px;
+}
+
+.shout_box .header .open_btn {
+	background: url(images/close_btn.png) no-repeat 0px -32px;
+	float: right;
+	width: 15px;
+	height: 15px;
+}
+.shout_box .header .open_btn:hover {
+	background: url(images/close_btn.png) no-repeat 0px -48px;
+}
+.shout_box .header{
+	padding: 5px 3px 5px 5px;
+	font: 11px 'lucida grande', tahoma, verdana, arial, sans-serif;
+	font-weight: bold;
+	color:#fff;
+	border: 1px solid rgba(0, 39, 121, .76);
+	border-bottom:none;
+	cursor: pointer;
+}
+.shout_box .header:hover{
+	background-color: #627BAE;
+}
+.shout_box .message_box {
+	background: #FFFFFF;
+	height: 200px;
+	overflow:auto;
+	border: 1px solid #CCC;
+}
+.shout_msg{
+	margin-bottom: 10px;
+	display: block;
+	border-bottom: 1px solid #F3F3F3;
+	padding: 0px 5px 5px 5px;
+	font: 11px 'lucida grande', tahoma, verdana, arial, sans-serif;
+	color:#7C7C7C;
+}
+.message_box:last-child {
+	border-bottom:none;
+}
+time{
+	font: 11px 'lucida grande', tahoma, verdana, arial, sans-serif;
+	font-weight: normal;
+	float:right;
+	color: #D5D5D5;
+}
+.shout_msg .username{
+	margin-bottom: 10px;
+	margin-top: 10px;
+}
+.user_info input {
+	width: 98%;
+	height: 25px;
+	border: 1px solid #CCC;
+	border-top: none;
+	padding: 3px 0px 0px 3px;
+	font: 11px 'lucida grande', tahoma, verdana, arial, sans-serif;
+}
+.shout_msg .username{
+	font-weight: bold;
+	display: block;
+}
+-->
+</style>
+<script type="text/javascript">
+$(document).ready(function() {
+
+	
+$('.toggle_chat').css('display','none');
+	// load messages every 1000 milliseconds from server.
+	load_data = {'fetch':1};
+	window.setInterval(function(){
+	 $.post('shout.php', load_data,  function(data) {
+		$('.message_box').html(data);
+		var scrolltoh = $('.message_box')[0].scrollHeight;
+		$('.message_box').scrollTop(scrolltoh);
+	 });
+	}, 1000);
+	
+	//method to trigger when user hits enter key
+	$("#shout_message").keypress(function(evt) {
+		if(evt.which == 13) {
+				var iusername = $('#shout_username').val();
+				var imessage = $('#shout_message').val();
+				post_data = {'username':iusername, 'message':imessage};
+			 	
+				//send data to "shout.php" using jQuery $.post()
+				$.post('shout.php', post_data, function(data) {
+					
+					//append data into messagebox with jQuery fade effect!
+					$(data).hide().appendTo('.message_box').fadeIn();
+	
+					//keep scrolled to bottom of chat!
+					var scrolltoh = $('.message_box')[0].scrollHeight;
+					$('.message_box').scrollTop(scrolltoh);
+					
+					//reset value of message box
+					$('#shout_message').val('');
+					
+				}).fail(function(err) { 
+				
+				//alert HTTP server error
+				alert(err.statusText); 
+				});
+			}
+	});
+	
+	//toggle hide/show shout box
+	$(".close_btn").click(function (e) {
+		//get CSS display state of .toggle_chat element
+		var toggleState = $('.toggle_chat').css('display');
+		
+		//toggle show/hide chat box
+		$('.toggle_chat').slideToggle();
+		
+		//use toggleState var to change close/open icon image
+		if(toggleState == 'block')
+		{
+			$(".header div").attr('class', 'open_btn');
+		}else{
+			$(".header div").attr('class', 'close_btn');
+		}
+		 
+		 
+	});
+});
+
+</script>
 	<style type="text/css">
   			#slideshow .c_slider {
 				position: absolute;
@@ -421,8 +569,19 @@ include_once 'languages/'.$lang_file;
 			<a href="http://info.flagcounter.com/UfWP"><img src="http://s06.flagcounter.com/count2/UfWP/bg_000000/txt_F7FDFF/border_381D38/columns_2/maxflags_10/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
 			</div>
 		</section>
-	</aside>
 
+	</aside>
+	<div class="shout_box">
+<div class="close_btn"><div class="header">Shoutbox </div></div>
+  <div class="toggle_chat">
+  <div class="message_box">
+    </div>
+    <div class="user_info">
+    <input name="shout_username" id="shout_username" type="text" placeholder="Your Name" maxlength="15" />
+   <input name="shout_message" id="shout_message" type="text" placeholder="Type Message Hit Enter" maxlength="100" /> 
+    </div>
+    </div>
+</div>
 	</div>
 
 	<footer id="rodape">
@@ -443,6 +602,11 @@ include_once 'languages/'.$lang_file;
 	<script type="text/javascript" src="js/lightbox/js/lightbox.js"></script>
     <script type="text/javascript" src="js/jquery.js" ></script>
     <script type="text/javascript" src="js/jquery.maskMoney.js" ></script>
+	<!-- Include jQuery and the EmojiOne library -->
+    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://cdn.jsdelivr.net/emojione/1.3.0/lib/js/emojione.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+
     <script type="text/javascript">
         $(document).ready(function(){
               $("input.dinheiro").maskMoney({showSymbol:true, symbol:"ā‚¬", decimal:",", thousands:"."});
