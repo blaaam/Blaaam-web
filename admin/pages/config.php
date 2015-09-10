919e25ad723834c14d9fb8063e0225afd58a39d8
SmokeY <staff.migets@gmail.com>
2015-09-10 17:55:16 +0300

    added

---
+++ admin/pages/config.php
@@ -0,0 +1,174 @@
+<?php if(!isset($pros)){echo 'Protected Page'; exit;};?>
+ <form method="post" name="config_form">
+        <div id="field">
+		<table border="0">
+            <tr>
+				<td>Path to Login server log file :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Path to Game server log file :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Write your server name here :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Server rate XP, will be displayed in top-bar at web pages :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Server rate SP, will be displayed in top-bar at web pages :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Server rate DROP, will be displayed in top-bar at web pages :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Server rate ADENA, will be displayed in top-bar at web pages :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Server rate SPOIL, will be displayed in top-bar at web pages :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Path to logo file (Not used currently) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Path to image for first slide in web page :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Path to image for second slide in web page :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Path to image for third slide in web page :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner name for first vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner name link for second vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner name link for third vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner name link for forth vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner name link for five vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner name link for six vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner image for first vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner image for second vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner image for third vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner image for forth vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner image for five vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner image for six vote (Only image) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner address link for first vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner address link for second vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner address link for third vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner address link for forth vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>VOTE Site banner address link for five vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>VOTE Site banner address link for six vote (Only address) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Credits which will be displayed at bottom at web in the footer section :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download System map mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download System map mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download Animations map mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download Animations map mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download System textures map mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download System textures map mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download full patch installation mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download full patch installation mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download full patch archived mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download full patch archived mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Site to download game client mirror 1 :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site to download game client mirror 2 :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Title for meta tag  :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Site e-mail :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Database (Will add more of database entry's here) :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+			<tr>
+				<td>Not sure where this is used, will check later! :</td>
+				<td><input type="text" name="fname" value=""></td>
+				<td>&nbsp;</td>
+				<td>Max enchant rate for your server :</td>
+				<td><input type="text" name="fname" value=""></td>
+			</tr>
+        </div>
+</form>
